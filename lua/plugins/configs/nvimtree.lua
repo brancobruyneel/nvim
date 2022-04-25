30faa63e9a6d38a22c7a1eab1285af490e5449de
Branco <branco.bruyneel@gmail.com>
2022-04-25 10:42:02 +0200

    feat(nvimtree): clean up

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -35,30 +35,22 @@ g.nvim_tree_icons = {
 }
 
 nvimtree.setup {
-  filters = {
-    dotfiles = false,
-  },
   disable_netrw = true,
-  hijack_netrw = true,
-  ignore_ft_on_setup = { "dashboard" },
-  auto_close = false,
-  open_on_tab = false,
+  hide_root_folder = true,
   hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
+  hijack_netrw = true,
   update_cwd = true,
   update_focused_file = {
     enable = true,
     update_cwd = false,
   },
   view = {
-    allow_resize = true,
     side = "left",
     width = 25,
-    hide_root_folder = true,
     preserve_window_proportions = true,
   },
-  git = {
-    enable = false,
-    ignore = false,
+  filters = {
+    custom = { ".git" },
+    exclude = { ".gitignore" },
   },
 }
