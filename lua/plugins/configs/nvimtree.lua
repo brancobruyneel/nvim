7c8ccebdcc0bec61b8d38fc2303717bfcd173424
Branco <branco.bruyneel@gmail.com>
2022-06-02 09:02:24 +0200

    fix(nvimtree)

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -6,34 +6,6 @@ end
 
 local g = vim.g
 
-g.nvim_tree_show_icons = {
-  folders = 1,
-  files = 1,
-  git = 1,
-}
-
-g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    deleted = "",
-    ignored = "◌",
-    renamed = "➜",
-    staged = "✓",
-    unmerged = "",
-    unstaged = "✗",
-    untracked = "★",
-  },
-  folder = {
-    default = "",
-    empty = "",
-    empty_open = "",
-    open = "",
-    symlink = "",
-    symlink_open = "",
-  },
-}
-
 nvimtree.setup {
   auto_reload_on_write = true,
   disable_netrw = true,
@@ -43,6 +15,36 @@ nvimtree.setup {
   update_focused_file = {
     enable = true,
     update_cwd = false,
+  },
+  renderer = {
+    icons = {
+      show = {
+        folder = true,
+        file = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          deleted = "",
+          ignored = "◌",
+          renamed = "➜",
+          staged = "✓",
+          unmerged = "",
+          unstaged = "✗",
+          untracked = "★",
+        },
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      },
+    },
   },
   view = {
     hide_root_folder = true,
