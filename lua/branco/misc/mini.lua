afa325e66a6406febcaab1b45fca971e744b0131
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-05-04 11:55:03 +0200

    chore: fmt

--- lua/branco/misc/mini.lua
+++ lua/branco/misc/mini.lua
@@ -1,12 +1,12 @@
-require("lze").load({
+require("lze").load {
   {
     "mini.surround",
     event = "DeferredUIEnter",
     after = function(_)
-      require("mini.surround").setup({
+      require("mini.surround").setup {
         respect_selection_type = true,
         search_method = "cover_or_nearest",
-      })
+      }
     end,
-  }
-})
+  },
+}
