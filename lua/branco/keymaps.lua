5bf84204e1876017e32e45fdfde439c4522c31f8
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-05-04 11:55:48 +0200

    fix: mappings

--- lua/branco/keymaps.lua
+++ lua/branco/keymaps.lua
@@ -1,13 +1,30 @@
 local map = vim.keymap.set
 
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+
 map("n", "<c-j>", "<c-w><c-j>")
 map("n", "<c-k>", "<c-w><c-k>")
 map("n", "<c-l>", "<c-w><c-l>")
 map("n", "<c-h>", "<c-w><c-h>")
 
 for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-	map("n", string.format("<leader>%d", idx), "<CMD>")
+  map("n", string.format("<leader>%d", idx), "<CMD>")
 end
+
+-- tabufline
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+
+map("n", "<tab>", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "<S-tab>", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+map("n", "<leader>x", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
 
 -- git
 map("n", "<leader>gp", "<CMD> G pull <CR>", { desc = "git pull" })
@@ -19,6 +36,6 @@ map("v", "<leader>y", '"+y', { desc = "visual copy to system clipboard" })
 -- nvimtree
 map("n", "<leader>ne", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
 
-map('i', '<C-l>', function()
-	vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-end, { desc = 'Copilot Accept', noremap = true, silent = true })
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
--- lua/branco/ui/telescope.lua
+++ lua/branco/ui/telescope.lua
@@ -12,6 +12,7 @@ require("lze").load {
     { "<leader>fp", tf "buffers", desc = "Search Buffers" },
     { "<leader>fd", tf "diagnostics", desc = "Search Diagnostics (All)" },
     { "<leader>fD", tf("diagnostics", { bufnr = 0 }), desc = "Search Diagnostics (Buffer)" },
+    { "<leader>fh", tf "help_tags", desc = "Search help pages" },
     { "<leader>fH", tf "highlights", desc = "Search Highlight Groups" },
     { "<leader>fj", tf "jumplist", desc = "Search Jump List" },
     { "<leader>fk", tf "keymaps", desc = "Search Key Mappings" },
@@ -20,25 +21,11 @@ require("lze").load {
     { "<leader>fo", tf "oldfiles", desc = "Search Old (Recent) Files" },
     { "<leader>fq", tf "quickfix", desc = "Search Quickfix List" },
     { "<leader>fw", tf "live_grep", desc = "Search Words", mode = { "n", "x" } },
-    { "<leader>f.", tf "current_buffer_fuzzy_find", desc = "Search Buffer Lines" },
-    { "<leader>f/", tf "search_history", desc = "Search History" },
-    { "<leader>f'", tf "marks", desc = "Search Marks" },
-    { '<leader>f"', tf "registers", desc = "Search Registers" },
-    { "<leader>f<cr>", tf "resume", desc = "Resume Last Search" },
-    -- Git
-    { "<leader>gb", tf "git_branches", desc = "Search Git Branches" },
+
+    { "<leader>fb", tf "git_branches", desc = "Search Git Branches" },
     { "<leader>gs", tf "git_status", desc = "Search Git Status" },
-    { "<leader>gS", tf "git_stash", desc = "Search Git Stash" },
     { "<leader>gl", tf "git_commits", desc = "Search Git Commits" },
     { "<leader>gf", tf "git_bcommits", desc = "Search Git Buffer Commits" },
-    -- LSP
-    { "gd", tf "lsp_definitions", desc = "Go to Definition" },
-    { "gD", tf "lsp_declarations", desc = "Go to Declaration" },
-    { "gr", tf "lsp_references", nowait = true, desc = "Search References" },
-    { "gI", tf "lsp_implementations", desc = "Go to Implementation" },
-    { "gy", tf "lsp_type_definitions", desc = "Go to Type Definition" },
-    -- Miscellaneous
-    { "<leader>uC", tf "colorscheme", desc = "Search Colorschemes" },
   },
   before = function(_)
     dofile(vim.g.base46_cache .. "telescope")
