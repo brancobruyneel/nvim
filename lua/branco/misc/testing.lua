12b83ac0b9f5cd974be8b4c7d02a1cc32f1a5a8d
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-05-06 16:47:31 +0200

    fix(neotest): load neotest-golang

--- lua/branco/misc/testing.lua
+++ lua/branco/misc/testing.lua
@@ -1,87 +1,92 @@
 require("lze").load {
-  "neotest",
-  cmd = "Neotest",
-  lazy = false,
-  keys = {
-    {
-      "<leader>tf",
-      function()
-        require("neotest").run.run(vim.fn.expand "%")
-      end,
-      desc = "<neotest> Test current file",
-    },
-    {
-      "<leader>tl",
-      function()
-        require("neotest").run.run_last()
-      end,
-      desc = "<neotest> Test last",
-    },
-    {
-      "<leader>tw",
-      function()
-        require("neotest").watch.toggle(vim.fn.expand "%")
-      end,
-      desc = "<neotest> Watch current file",
-    },
-    {
-      "<leader>tn",
-      function()
-        require("neotest").run.run()
-      end,
-      desc = "<neotest> Run close test",
-    },
-    {
-      "<leader>ts",
-      function()
-        require("neotest").run.stop()
-      end,
-      desc = "<neotest> Stop test",
-    },
-    {
-      "<leader>te",
-      function()
-        require("neotest").summary.toggle()
-      end,
-      desc = "<neotest> Toggle summary",
-    },
-    {
-      "<leader>td",
-      function()
-        require("neotest").run.run { suite = true, strategy = "dap" }
-      end,
-      desc = "Debug nearest test",
-    },
-    {
-      "<leader>to",
-      function()
-        require("neotest").output_panel.toggle()
-      end,
-      desc = "<neotest> Toggle output panel",
-    },
-    {
-      "[t",
-      function()
-        require("neotest").jump.prev()
-      end,
-      desc = "<neotest> Jump to previous test",
-    },
-    {
-      "]t",
-      function()
-        require("neotest").jump.next()
-      end,
-      desc = "<neotest> Jump to next test",
-    },
-  },
-  after = function(_)
-    require("neotest").setup {
-      adapters = {
-        require "neotest-golang" {
-          dap_go_enabled = true,
-          testify_enabled = true,
-        },
+  {
+    "neotest",
+    cmd = "Neotest",
+    keys = {
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "<neotest> Test current file",
       },
-    }
-  end,
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "<neotest> Test last",
+      },
+      {
+        "<leader>tw",
+        function()
+          require("neotest").watch.toggle(vim.fn.expand "%")
+        end,
+        desc = "<neotest> Watch current file",
+      },
+      {
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "<neotest> Run close test",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "<neotest> Stop test",
+      },
+      {
+        "<leader>te",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "<neotest> Toggle summary",
+      },
+      {
+        "<leader>td",
+        function()
+          require("neotest").run.run { suite = true, strategy = "dap" }
+        end,
+        desc = "Debug nearest test",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "<neotest> Toggle output panel",
+      },
+      {
+        "[t",
+        function()
+          require("neotest").jump.prev()
+        end,
+        desc = "<neotest> Jump to previous test",
+      },
+      {
+        "]t",
+        function()
+          require("neotest").jump.next()
+        end,
+        desc = "<neotest> Jump to next test",
+      },
+    },
+    after = function(_)
+      require("neotest").setup {
+        adapters = {
+          require "neotest-golang" {
+            dap_go_enabled = true,
+            testify_enabled = true,
+          },
+        },
+      }
+    end,
+  },
+  {
+    "neotest-golang",
+    on_plugin = "neotest",
+  },
 }
